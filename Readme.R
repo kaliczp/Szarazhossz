cf52a3c70940140d0c525699014b1adedd996109
Péter Kalicz <kaliczp@gmail.com>
2016-11-15 00:21:36 +0100

    Initial commit
    Éves csapadékok generálása. Szárazidőszak számítási ötlet.

---
+++ Readme.R
@@ -0,0 +1,26 @@
+## Cél, egy 1 oszlopos txt-ből (napi csapadékok) számolja ki a
+## leghosszabb csapadékszegény időszak hosszát adott évre (és ennek 30
+## éves átlagát).  Kérdés az is, hogy egy 1 oszlopos txt mellé
+## (pl. csapadékadatok) lehet-e tetetni az R-rel év és hónap
+## oszlopokat.  Erről tudnánk majd beszélni?
+
+## a csapadékszegény időszak gondolom az lehet, hogy hány napig egymás
+## után van 1 mm-nél kevesebb eső - amúgy PAI-t szeretnék számolni,
+## annak az egyik korrekciós faktorához kell, pontosabban ott se
+## definiálták
+
+## Napi csapadék generálás
+Time <- seq(as.Date("2015-01-01"),as.Date("2016-12-31"),"days")
+library(xts)
+TS.xts <- xts(rep.int(0, times=length(Time)), Time)
+csapi <- sample(1:length(Time), size=0.3*length(Time))
+TS.xts[csapi] <- round(rf(length(csapi),20,1),1)
+ttbig <- length(TS.xts[TS.xts >100])
+TS.xts[TS.xts >100] <- round(rnorm(ttbig, mean=80, sd=10),1)
+plot(TS.xts,typ="h")
+
+## Nagyobb-e 1 mm-nél
+szarazTS.xts <- TS.xts < 1
+szaraz.diff <- diff(szarazTS.xts)
+szaraz.idx <- which(szaraz.diff == -1)
+max(period.apply(szarazTS.xts, szaraz.idx, sum))
